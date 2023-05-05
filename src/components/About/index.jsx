4ff76fad88047eed5f64b7a35f25cf01442788b1
Ben <ben.ahlander@gmail.com>
2023-05-05 11:02:44 -0600

    update marquee

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,32 +1,74 @@
 import Grid from "@material-ui/core/Grid";
 import React from "react";
 import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
 const aboutText =
   "I am a certified full stack web developer with over five years of full-time font-end coding experience. I have specialized in writing front-end web applications with scalable, component architecture. I spend most of my time working with ReactJS, Javascript, and attending code meetups. I like to use modern technology to build apps that look great and are creative. I enjoy learning about new technologies and studying topics like Machine Learning and Blockchain.";
 
-const About = () => (
-  <Grid
-    container
-    style={{
-      backgroundColor: "#607d8b",
-      paddingTop: "16px",
-      paddingBottom: "16px",
-      paddingRight: "16px",
-      paddingLeft: "16px",
-    }}
-    alignContent="center"
-    justify="center"
-  >
-    <Grid item xs={12} sm={10} md={8}>
-      <Typography align="left" style={{ color: "white" }}>
-        ABOUT
-      </Typography>
-      <Typography align="left" style={{ color: "white" }}>
-        {aboutText}
-      </Typography>
+const useStyles = makeStyles((theme) => {
+  return {
+    portrait: {
+      background:
+        "url(https://storage.googleapis.com/resume_assets/benportrait.jpeg) no-repeat center top",
+      backgroundSize: "cover",
+      border: "5px solid white",
+      borderRadius: "50%",
+      position: "relative",
+      height: "150px",
+      width: "150px",
+      top: "-135px",
+    },
+    flexCenterColumn: {
+      display: "flex",
+      height: 0,
+      flexDirection: "row",
+      justifyContent: "flex-start",
+      alighItems: "flex-start",
+      [theme.breakpoints.up("md")]: {
+        display: "none",
+      },
+    },
+    name: {
+      color: "white",
+      marginTop: "40px",
+      marginBottom: "16px",
+      [theme.breakpoints.up("md")]: {
+        display: "none",
+      },
+    },
+  };
+});
+
+const About = () => {
+  const classes = useStyles();
+
+  return (
+    <Grid
+      container
+      style={{
+        backgroundColor: "#607d8b",
+        padding: "40px",
+      }}
+      alignContent="center"
+      justify="flex-end"
+    >
+      <Grid item xs={12} sm={10} md={7}>
+        <div className={classes.flexCenterColumn}>
+          <div className={classes.portrait} />
+        </div>
+        <Typography variant="h3" className={classes.name}>
+          Ben Ahlander
+        </Typography>
+        <Typography align="left" style={{ color: "white" }}>
+          ABOUT
+        </Typography>
+        <Typography align="left" style={{ color: "white" }}>
+          {aboutText}
+        </Typography>
+      </Grid>
     </Grid>
-  </Grid>
-);
+  );
+};
 
 export default About;
--- src/components/Marquee/index.jsx
+++ src/components/Marquee/index.jsx
@@ -1,21 +1,55 @@
 import Grid from "@material-ui/core/Grid";
 import React from "react";
-import Typography from "@material-ui/core/Typography";
+import { Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => {
-  const isDarkTheme = theme.palette.type === "dark";
   return {
     root: {
       padding: theme.spacing(3, 2),
     },
     background: {
-      background: `url(https://storage.googleapis.com/resume_assets/header-bg2.jpg) no-repeat center top;`,
+      background:
+        "url(https://storage.googleapis.com/resume_assets/codioful-formerly-gradienta-G084bO4wGDA-unsplash.jpg) no-repeat center top",
       backgroundSize: "cover",
-      height: "600px",
+      height: "300px",
       maxHeight: "80vh",
-      filter: isDarkTheme ? "grayscale(100%)" : null,
-      padding: "16px",
+      padding: "32px",
+      [theme.breakpoints.down("sm")]: {
+        height: "150px",
+      },
+    },
+    portrait: {
+      background:
+        "url(https://storage.googleapis.com/resume_assets/benportrait.jpeg) no-repeat center top",
+      backgroundSize: "cover",
+      border: "5px solid white",
+      borderRadius: "50%",
+      height: "300px",
+      width: "300px",
+      position: "relative",
+      top: "100px",
+      [theme.breakpoints.down("sm")]: {
+        height: "150px",
+        width: "150px",
+        top: "50px",
+      },
+    },
+    flexCenterColumn: {
+      display: "flex",
+      justifyContent: "center",
+      flexDirection: "column",
+      alignItems: "center",
+      [theme.breakpoints.down("sm")]: {
+        display: "none",
+      },
+    },
+    name: {
+      color: "white",
+      marginBottom: "32px",
+      [theme.breakpoints.down("sm")]: {
+        display: "none",
+      },
     },
   };
 });
@@ -24,24 +58,25 @@ export default function PaperSheet() {
   const classes = useStyles();
 
   return (
-    <Grid
-      container
-      className={classes.background}
-      alignItems="center"
-      justify="center"
-      alignContent="center"
-    >
-      <Grid item xs={12}>
-        <Typography style={{ color: "white" }} variant="h1" align="center">
-          Ben Ahlander
-        </Typography>
+    <>
+      <Grid
+        container
+        className={classes.background}
+        alignItems="flex-end"
+        justify="center"
+        alignContent="center"
+      >
+        <Grid item xs={12} sm={4} md={5}>
+          <div className={classes.flexCenterColumn}>
+            <div className={classes.portrait} />
+          </div>
+        </Grid>
+        <Grid item xs={7} sm={8} md={7}>
+          <Typography className={classes.name} variant="h3">
+            Ben Ahlander
+          </Typography>
+        </Grid>
       </Grid>
-      <Grid item xs={11} sm={8} md={6}>
-        <Typography style={{ color: "white" }} variant="h4" align="center">
-          Full stack engineer specializing in scalable, front end, component
-          architecture.
-        </Typography>
-      </Grid>
-    </Grid>
+    </>
   );
 }
